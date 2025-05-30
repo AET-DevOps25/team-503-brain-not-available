13be45bf6e1655956b479c7a3d603fb5d7a47be9
Manuel Kienlein <manuel.kienlein@gmx.de>
2025-05-30 15:39:22 +0200

    Added vue-router

---
+++ client/src/router/index.ts
@@ -0,0 +1,18 @@
+// src/router/index.ts
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Importiere deine Seitenkomponenten
+import PageView from '@/views/PageView.vue'
+
+const routes = [
+    { path: '/', name: 'Dashboard', component: PageView },
+    { path: '/settings', name: 'Settings', component: PageView },
+    { path: '/profile', name: 'Profile', component: PageView },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
