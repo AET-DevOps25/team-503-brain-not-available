e38d7bc302aeecad0660bde66d3973f017edad01
Felix Hahn <->
2025-06-21 13:49:22 +0200

    Don't use production database for tests

--- backend/src/test/java/com/wiki/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/wiki/backend/BackendApplicationTests.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class BackendApplicationTests {
 
 	@LocalServerPort
