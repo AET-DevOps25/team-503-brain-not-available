174b53ecd936c1500040f576e86f81942deefc1a
Manuel Kienlein <manuel.kienlein@gmx.de>
2025-07-01 21:10:43 +0200

    Added login view

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+</script>
+
+<template>
+<div class="flex h-screen w-full items-center justify-center px-4 bg-gray-100">
+  <Card class="w-full max-w-sm">
+    <CardHeader>
+      <CardTitle class="text-2xl">
+        Login
+      </CardTitle>
+      <CardDescription>
+        Enter your email below to login to your account.
+      </CardDescription>
+    </CardHeader>
+    <CardContent class="grid gap-4">
+      <div class="grid gap-2">
+        <Label for="email">Email</Label>
+        <Input id="email" type="email" placeholder="john.doe@tum.com" required />
+      </div>
+      <div class="grid gap-2">
+        <Label for="password">Password</Label>
+        <Input id="password" type="password" required />
+      </div>
+    </CardContent>
+    <CardFooter class="grid gap-4">
+      <div class="grid gap-2">
+        <Button class="w-full">
+            Sign in
+        </Button>
+      </div>
+      <div class="grid gap-2">
+        <div class="mt-4 text-center text-sm">
+            You need an account?
+            <a href="#" class="underline">
+                Sign up
+            </a>
+        </div>
+      </div>
+    </CardFooter>
+  </Card>
+</div>
+</template>
