4de6039fbf57b3e76654a1a219d8a27c0839d4ac
Manuel Kienlein <manuel.kienlein@gmx.de>
2025-07-01 19:59:16 +0200

    Fixed buggy sidebar and improved page view design

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import AppSidebar from '@/components/AppSidebar.vue'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+
 </script>
 
 <!--<template>
@@ -17,26 +15,5 @@ import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 </template>-->
 
 <template>
-    <SidebarProvider>
-        <AppSidebar />
-        <main>
-            <SidebarTrigger />
-            <RouterView />
-        </main>
-    </SidebarProvider>
+  <RouterView />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- client/src/views/PageView.vue
+++ client/src/views/PageView.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { Separator } from '@/components/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+
 import { ref, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { pageService, type Page } from '@/service/pageService'
@@ -23,15 +39,40 @@ watch(
 </script>
 
 <template>
-  <div v-if="page">
-    <h1>{{ page.title }}</h1>
-    <p>{{ page.content }}</p>
-  </div>
-  <div v-else>
-    <p>Lade Seite…</p>
-  </div>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <SidebarTrigger class="-ml-1" />
+        <Separator orientation="vertical" class="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem class="hidden md:block">
+              <BreadcrumbLink href="#">
+                Wiki
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator class="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{{ page.title }}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+      <div class="flex flex-1 flex-col gap-4 p-4">
+        <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min p-5">
+          <div v-if="page">
+              <h1>{{ page.title }}</h1>
+              <p>{{ page.content }}</p>
+          </div>
+          <div v-else>
+              <p>Lade Seite…</p>
+          </div>
+        </div>
+      </div>
+      <div id="page-wrapper" class="w-full">
+        
+    </div>
+    </SidebarInset>
+  </SidebarProvider>
 </template>
-
-<style scoped>
-
-</style>
